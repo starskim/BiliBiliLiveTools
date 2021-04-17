0162374dcbf8eebfbe5f872258feb2e3b1831fc0
Stars_Kim <Stars_Kim@163.com>
2021-04-17 23:48:37 +0800

    简化初始配置文件

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -1,39 +1,40 @@
 import config from "./config"
 
 const version = require('../../package.json')
+let envs = process.env
 
 const init = () => {
     if (config.get('version') !== version.version) {
         config.store = {
             version: version.version,
-            debug: process.env.DEBUG === 'true',
+            debug: envs.DEBUG === 'true',
             bilibiliInfo: {
                 uid: '',
-                username: process.env.USERNAME || '',
-                password: process.env.PASSWORD || '',
-                access_token: process.env.ACCESS_TOKEN || '',
-                refresh_token: process.env.REFRESH_TOKEN || '',
+                username: envs.USERNAME || '',
+                password: envs.PASSWORD || '',
+                access_token: envs.ACCESS_TOKEN || '',
+                refresh_token: envs.REFRESH_TOKEN || '',
             },
             StreamInfo: {
                 uid: '',
-                room_id: process.env.ROOM_ID || '',
+                room_id: envs.ROOM_ID || '',
             },
             UploaderInfo: {
-                uploadLocalFile: process.env.UPLOADLOCALFILE || true,
-                deleteLocalFile: process.env.DELETELOCALFILE || false,
-                videoPartLimitSize: process.env.VIDEOPARTLIMITSIZE || 100,
-                copyright: process.env.COPYRIGHT || 2,
-                title: process.env.TITLE || '',
-                tid: process.env.TID || 27,
-                tags: process.env.TAGS ? process.env.TAGS.split(',') : ['录播', '直播录像'],
-                desc: process.env.DESC || '',
-                cover: process.env.COVER || '',
-                no_reprint: process.env.NO_REPRINT || 1,
-                open_elec: process.env.OPEN_ELEC || 0
+                uploadLocalFile: envs.UPLOADLOCALFILE || true,
+                deleteLocalFile: envs.DELETELOCALFILE || false,
+                videoPartLimitSize: envs.VIDEOPARTLIMITSIZE || 100,
+                copyright: envs.COPYRIGHT || 2,
+                title: envs.TITLE || '',
+                tid: envs.TID || 27,
+                tags: envs.TAGS ? envs.TAGS.split(',') : ['录播', '直播录像'],
+                desc: envs.DESC || '',
+                cover: envs.COVER || '',
+                no_reprint: envs.NO_REPRINT || 1,
+                open_elec: envs.OPEN_ELEC || 0
             },
             DingTalk: {
-                webhook: process.env.DINGTALKWEBHOOK || '',
-                secret: process.env.DINGTALKSECRET || '',
+                webhook: envs.DINGTALKWEBHOOK || '',
+                secret: envs.DINGTALKSECRET || '',
             },
         }
     }
